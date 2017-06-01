3270cea46a137163de3794994871571258d9c6fa
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2017-06-01 14:35:54 +0200

    Fix small error

--- tests/routes.js
+++ tests/routes.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
     app.get("/tests/advanced/test.authorization.json", function(req, res) {
-        if (!("x-auth-token" in req.headers)) {
+        if ("x-auth-token" in req.headers) {
+            res.status(200).json({response: "Your settings are saved"});
+        } else {
             res.status(403).json({error: "no_auth_token"});
         }
-
-        res.status(200).json({response: "Your settings are saved"});
     })
 }
