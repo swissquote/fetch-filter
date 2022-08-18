3bb87e7348b78ca2ee300f55384187f952a41d95
Stéphane Goetz <stephane.goetz@swissquote.ch>
2022-08-18 16:08:33 +0200

    Don't include dist

--- dist/FilterChain.d.ts
+++
@@ -1,7 +0,0 @@
-import { FetchFilter } from "./index";
-declare const _default: {
-    before: (filters: FetchFilter[], settings: RequestInfo, options?: RequestInit | undefined) => void;
-    done: (filters: FetchFilter[], args: any[], callback: (newResponse: any) => void) => void;
-    fail: (filters: FetchFilter[], args: any[], callback: () => void) => void;
-};
-export default _default;
